a979632fc23b1f722d1f740cbcac9282b9adfd40
hongjianqiang <569250030@qq.com>
2016-12-05 00:04:45 +0800

    编写新功能

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@
 
 'use strict';
 
+var COUNTER = 0;
+
 var Data = {
+  columnChooseStatus: 0,
   itemsChooseStatus: 0
 };
 
@@ -13,6 +16,7 @@ Data['boxCSS'] = {
   'display': ['none', 'flex', 'inline-flex', '-webkit-flex'],
   'flex-direction': ['row', 'row-reverse', 'column', 'column-reverse'],
   'flex-wrap': ['nowrap', 'wrap', 'wrap-reverse'],
+  'flex-flow': '',
   'justify-content': ['flex-start', 'flex-end', 'center', 'space-between', 'space-around'],
   'align-items': ['flex-start', 'flex-end', 'center', 'baseline', 'stretch'],
   'align-content': ['flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'stretch']
@@ -38,19 +42,60 @@ Data['itemCSS']= {
 };
 
 Data['itemsArray'] = [
-  {
-    'order': 0,
-    'flex-grow': 0,
-    'flex-shrink': 1,
-    'flex-basis': 'auto',
-    'align-self': 'auto'
-  }
+  [
+    {
+      'order': 0,
+      'flex-grow': 0,
+      'flex-shrink': 1,
+      'flex-basis': 'auto',
+      'align-self': 'auto'
+    },
+    {
+      'order': 0,
+      'flex-grow': 0,
+      'flex-shrink': 1,
+      'flex-basis': 'auto',
+      'align-self': 'auto'
+    }
+  ],
+  [
+    {
+      'order': 0,
+      'flex-grow': 0,
+      'flex-shrink': 1,
+      'flex-basis': 'auto',
+      'align-self': 'auto'
+    }
+  ],
+  [
+    {
+      'order': 0,
+      'flex-grow': 0,
+      'flex-shrink': 1,
+      'flex-basis': 'auto',
+      'align-self': 'auto'
+    },
+    {
+      'order': 0,
+      'flex-grow': 0,
+      'flex-shrink': 1,
+      'flex-basis': 'auto',
+      'align-self': 'auto'
+    }
+  ]
 ];
 
 var LearnFlex = new Vue({
   el: '#LearnFlex',
   data: Data,
   methods: {
+    counter: function() {
+      return ++COUNTER;
+    },
+    clearCounter: function() {
+      console.log('Called clearCounter()');
+      COUNTER = 0;
+    },
     selectedBoxCSS: function(e) {
       Data['boxSelectStyle'][e.target.name] = e.target.value;
     },
@@ -81,24 +126,60 @@ var LearnFlex = new Vue({
       var n = this.itemsArray.length;
 
       for (; m > n; n++) {
-        this.itemsArray.push({
+        this.itemsArray.push([{
           'order': 0,
           'flex-grow': 0,
           'flex-shrink': 1,
           'flex-basis': 'auto',
           'align-self': 'auto'
-        });
+        }]);
       }
 
       for (; m < n; n--) {
         this.itemsArray.pop();
       }
     },
-    changeItemsIndex: function(e) {
-      //var itemsChooseStatus = this.itemsChooseStatus;
-      //delete(Data['itemCSS']['align-self']);
-      //Data['itemCSS']['align-self'].pop();
-      //console.log('delete');
+    getItemPos: function(num) {
+      var itemX = 0;
+      var itemY = 0;
+      var sum = 0;
+      var m = this.itemsArray.length;
+
+      for(var n = 0; n < m; n++) {
+        sum += this.itemsArray[n].length;
+        if (sum >= num) {
+          itemX = n;
+          break;
+        }
+      }
+
+      var i = this.itemsArray[itemX].length;
+
+      for(var n = 0; n < i; n++) {
+
+      }
+
+      return (itemX, itemY);
+    }
+  },
+  computed: {
+    flexFlow: function() {
+      var fF = this.boxSelectStyle['flex-direction'] + ' ' + this.boxSelectStyle['flex-wrap'];
+      if (fF == ' ') {
+        return 'flex-direction 和 flex-wrap 简写'
+      } else {
+        return fF;
+      }
+    },
+    itemsLength: function() {
+      var sum = 0;
+      var m = this.itemsArray.length;
+
+      for(var n = 0; n < m; n++) {
+        sum += this.itemsArray[n].length;
+      }
+      return sum;
     }
   }
 });
+
