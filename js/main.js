4604f6ba373874a5ebf4a9a8216c9bc02dda02ab
洪建强 <569250030@qq.com>
2016-12-06 16:00:35 +0800

    重新修改了数据结构

--- js/main.js
+++ js/main.js
@@ -5,6 +5,125 @@
 
 'use strict';
 
+var Data = {
+};
+
+Data['boxAttrs'] = {
+  'display': ['none', 'flex'],
+  'flex-direction': ['row', 'row-reverse', 'column', 'column-reverse'],
+  'flex-wrap': ['nowrap', 'wrap', 'wrap-reverse'],
+  'flex-flow': '',
+  'justify-content': ['flex-start', 'flex-end', 'center', 'space-between', 'space-around'],
+  'align-items': ['flex-start', 'flex-end', 'center', 'baseline', 'stretch'],
+  'align-content': ['flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'stretch']
+};
+
+Data['itemAttrs']= {
+  'order': 0,
+  'flex-grow': 0,
+  'flex-shrink': 1,
+  'flex-basis': 'auto',
+  'flex': '',
+  'align-self': ['auto', 'flex-start', 'flex-end', 'center', 'baseline', 'stretch']
+};
+
+Data['boxStyle']   = {
+  'display': 'flex',
+  'flex-flow': 'row nowrap',
+  'justify-content': 'flex-start',
+  'align-items': 'flex-start',
+  'align-content': 'stretch',
+  'width': '204px',
+  'height': '204px'
+};
+
+Data['columnStyle']= {
+  'display': 'none',
+  'flex-flow': 'row nowrap',
+  'justify-content': 'flex-start',
+  'align-items': 'flex-start',
+  'align-content': 'stretch'  
+};
+
+Data['itemStyle'] = {
+  'order': 0,
+  'flex': '0 1 auto',
+  'align-self': 'auto'
+};
+
+
+Data['itemsArr'] = [
+  [
+    {
+      'tag': 'A1',
+      'style': {'order': 0, 'flex': '0 1 auto', 'align-self': 'auto'}
+    },
+    {
+      'tag': 'A2',
+      'style': {'order': 0, 'flex': '0 1 auto', 'align-self': 'auto'}      
+    }
+  ],
+  [
+    {
+      'tag': 'A3',
+      'style': {'order': 0, 'flex': '0 1 auto', 'align-self': 'auto'}            
+    }
+  ],
+  [
+    {
+      'tag': 'A4',
+      'style': {'order': 0, 'flex': '0 1 auto', 'align-self': 'auto'}   
+    },
+    {
+      'tag': 'A5',
+      'style': {'order': 0, 'flex': '0 1 auto', 'align-self': 'auto'}         
+    }
+  ],
+];
+
+var LearnFlex = new Vue({
+  el: '#LearnFlex',
+  data: Data,
+  methods: {
+    getItemTag: function(n) {
+      /*
+       * @param {Number} n 给出.item的序号
+       * @return {String} 返回.item的tag
+       */
+      var sum = 0;
+      var L = this.itemsArr.length;
+
+      for (var i=0; i<L; i++) {
+        for(var j=0; j<this.itemsArr[i].length; j++){
+          if(n == sum){
+            return this.itemsArr[i][j]['tag'];
+            break;
+          }
+          if(sum>n) { break; }
+          sum++;
+        }
+      }
+
+      return null;
+    }
+  },
+  computed:{
+    itemsArrLen: function() {
+      /*
+       * @return {Number} 返回 itemsArr 数组长度
+       */
+      var sum = 0;
+      var L = this.itemsArr.length;
+
+      for(var i=0; i<L; i++) {
+        sum += this.itemsArr[i].length;
+      }
+      return sum;
+    }
+  }
+});
+
+/*
 var COUNTER = 0;
 var ITEM_X  = 0;
 var ITEM_Y  = 0;
@@ -164,10 +283,9 @@ var LearnFlex = new Vue({
       var m = this.itemsArray.length;
 
       if ('0' == itemsChooseStatus) {
-        /*
-         * 如果 itemsChooseStatus 全选
-         * 先把所有 item 移动到指定的 column
-         */
+        // 如果 itemsChooseStatus 全选
+        // 先把所有 item 移动到指定的 column
+
         for (var i=0; i<m; i++) {
           for(var j=0; j<this.itemsArray[i].length; j++){
             var tmp = this.itemsArray[i][j];
@@ -178,9 +296,8 @@ var LearnFlex = new Vue({
           }
         }
 
-        /*
-         * 再把所有 item 从 column 删除
-         */
+        // 再把所有 item 从 column 删除
+
         for (var i=0; i<m; i++) {
           for(var j=0; j<this.itemsArray[i].length; j++){
             var tmp = this.itemsArray[i][j];
@@ -207,9 +324,9 @@ var LearnFlex = new Vue({
       this.columnChooseStatus = ITEM_X;
     },
     setItemsNum: function(e) {
-      /*
-       * 修改 .item 数量
-       */
+
+       // 修改 .item 数量
+       
       var m = e.target.value;
       var n = this.itemsLength;
 
@@ -325,3 +442,4 @@ var LearnFlex = new Vue({
   }
 });
 
+*/
